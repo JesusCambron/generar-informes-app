39b2af1f149bf477fe67907a473ecb38ebcbc40b
Jesus Cambron <manuel.cam98@gmail.com>
2023-05-19 08:52:18 -0700

    Add select form util

---
+++ src/app/utils/forms/select/select.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css'],
+})
+export class SelectComponent {
+  @Input() selectName: string = '';
+  @Input() selectId: string = '';
+}
